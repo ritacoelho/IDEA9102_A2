f83fdacc5287993d4e8ddcd1e95c2b680c7a4309
ritacoelho <rita_coelho@icloud.com>
2023-05-21 15:41:40 +1000

    added remaining UI pages (about-the-project to be finished when mockup is finished)

--- phone_sender/public/sketch.js
+++ phone_sender/public/sketch.js
@@ -46,12 +46,15 @@ let selectedPole;
 let poles = [];
 
 //buttons
+let startButton;
+let aboutButton;
 let nextButton;
 let closeButton;
 
 //images
 let closeIcon;
 let micIcon;
+let bg;
 
 
 function preload(){
@@ -59,11 +62,15 @@ function preload(){
   closeIcon = loadImage('assets/icons8-close-96.png');
   //"https://icons8.com/icon/85796/microphone"
   micIcon = loadImage('assets/icons8-microphone-96.png');
+  //https://arts.lakemac.com.au/Venues/Multi-Arts-Pavilion-mima/Permanent-Artworks/The-Catenary
+  //pixelated using https://pinetools.com/pixelate-effect-image 
+  bg = loadImage('assets/background.png');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(25,16,50);
+  //background(25,16,50);
+  background(bg);
 
   //create & start an audio input
   mic = new p5.AudioIn();
@@ -75,18 +82,7 @@ function setup() {
 
   fft = new p5.FFT();
 
-  colorMode(HSB);
-  slider = createSlider(0, 360, 270);
-  slider.class("colorSelection");
-  //centering slider by subtracting half of it's screen size responsive width (70%)
-  slider.position((width/2) - width*.35, height*.85 - 150);
-  slider.input(updateUser);
-
-  nextButton = createButton("Next");
-  nextButton.position((width/2) - width*.35, height*.85);
-  nextButton.class("next-button");
-  nextButton.mousePressed(nextPressed);
-
+  //creating pole selection buttons
   for (let i = 0; i < 5; i++) {
     let xCoor;
     if(i%2 != 0) {
@@ -100,34 +96,100 @@ function setup() {
     poles[i].style("width: 100px; height: 100px; border-radius: 50px; background-color: white;");
   }
 
+  //creating slider
+  colorMode(HSB);
+  slider = createSlider(0, 360, 270);
+  slider.class("colorSelection");
+  //centering slider by subtracting half of it's screen size responsive width (70%)
+  slider.position((width/2) - width*.35, height*.85 - 150);
+  slider.input(updateUser);
+
+  //creating start button
+  startButton = createButton("Start");
+  startButton.position((width/2) - width*.35, height*.55);
+  startButton.class("next-button");
+  startButton.mousePressed(nextPressed);
+
+  //creating about button
+  aboutButton = createButton("About the project");
+  aboutButton.position((width/2) - width*.35, height*.65);
+  aboutButton.class("next-button");
+  aboutButton.mousePressed(aboutPage);
+
+  //creating next button
+  nextButton = createButton("Next");
+  nextButton.position((width/2) - width*.35, height*.85);
+  nextButton.class("next-button");
+  nextButton.mousePressed(nextPressed);
+
+  //creating close button
   closeButton = createImg('assets/icons8-close-96.png');
-  closeButton.position(30,30);
+  closeButton.position(width-140,60);
+  closeButton.class("close-button");
   closeButton.mousePressed(closePage);
 }
 
 
 function draw() {
+  background(bg);
 
-  push();
-    colorMode(RGB);
-    background(25,16,50);
-  pop();
-
-  //image(closeIcon, 30, 30, 75, 75);
+  // push();
+  //   colorMode(RGB);
+  //   background(25,16,50);
+  // pop();
   
-  if (currentScreen == 0) {
-    screen1();
-  } else if (currentScreen == 1) {
-    screen2();
-  } else if (currentScreen == 2) {
-    screen3();
+  switch (currentScreen) {
+    case -1:
+      fill(0,0,0,0.7);
+      rect(0,0,width, height);
+      fill(0,0,0,0.5);
+      rect(30,30,width-60, height-60);
+      aboutTheProject();
+      break;
+
+    case 0:
+      fill(0,0,0,0.7);
+      rect(30,30,width-60, height-60);
+      homepage();
+      break;
+
+    case 1: 
+      // fill(0,0,0,0.7);
+      // rect(0,0,width, height);
+      // fill(0,0,0,0.5);
+      // rect(30,30,width-60, height-60);
+      push();
+        colorMode(RGB);
+        background(25,16,50);
+      pop();
+      screen1();
+      break;
+    
+    case 2:
+      // fill(0,0,0,0.7);
+      // rect(0,0,width, height);
+      // fill(0,0,0,0.5);
+      // rect(30,30,width-60, height-60);
+      push();
+        colorMode(RGB);
+        background(25,16,50);
+      pop();
+      screen2();
+      break
+  
+    default:
+      fill(0,0,0,0.7);
+      rect(30,30,width-60, height-60);
+      homepage();
+      currentScreen = 0;
+      break;
   }
   
 }
 
 function nextPressed(){
-  captureAudio = true;
-  currentScreen = 1;
+  currentScreen++;
+  if(currentScreen == 2) captureAudio = true;
   console.log("next pressed");
 }
 
@@ -145,14 +207,81 @@ function selectPole(poleId) {
   console.log(selectedPole);
 }
 
-function screen1() {
+function aboutPage(){
+  currentScreen = -1;
+  console.log("about");
+}
+
+function homepage() {
+  startButton.show();
+  aboutButton.show();
   closeButton.hide();
-  selectedPole != undefined ? nextButton.show() : nextButton.hide();
-  selectedPole != undefined ? slider.show() : slider.hide();
+  nextButton.hide();
+  slider.hide();
+  poles.forEach(pole => {
+    pole.hide();
+  });
+
+  push();
+    noStroke();
+    fill(0,0,255);
+    textStyle(NORMAL);
+    textSize(160);
+    textAlign(CENTER, CENTER);
+    textFont('VT323');
+
+
+    text("SONIC\nLINK", width/2, height/4);
+
+    textSize(30);
+    text("Please allow microphone access on your device", width/2, height - 100);
+  pop();
+
+}
+
+function aboutTheProject() {
+  startButton.hide();
+  aboutButton.hide();
+  closeButton.show();
+  nextButton.hide();
+  slider.hide();
+  poles.forEach(pole => {
+    pole.hide();
+  });
+
+  push();
+    noStroke();
+    fill(0,0,255);
+    textStyle(NORMAL);
+    textSize(80);
+    textAlign(LEFT, CENTER);
+    textFont('VT323');
+
+
+    text("About the project", 60, 100);
+
+    // image(bg, 0, 0);
+    // noFill();
+    fill(0,0,255);
+    rect(60, 200, width-120, 400);
+
+    textSize(60);
+    text("Blablablabla", 60, 700);
+
+  pop();
+
+}
+
+function screen1() {
+  startButton.hide();
+  aboutButton.hide();
+  closeButton.show();
   poles.forEach(pole => {
     pole.show();
     pole.style("background-color: white;");
   });
+  selectedPole != undefined ? nextButton.show() : nextButton.hide();
+  selectedPole != undefined ? slider.show() : slider.hide();
 
   console.log("on screen 1");
   push();
@@ -162,9 +291,9 @@ function screen1() {
     noStroke();
     fill(255,255,255);
     textStyle(NORMAL);
-    textSize(40);
+    textSize(50);
     textAlign(CENTER, CENTER);
-    textFont('Verdana');
+    textFont('VT323');
 
     text("1. Pick your pole", width/4, -250 + height/4);
 
@@ -193,13 +322,14 @@ function screen1() {
 }
 
 function screen2() {
-
-  closeButton.show();
+  startButton.hide();
+  aboutButton.hide();
   nextButton.hide();
   slider.hide();
   poles.forEach(pole => {
     pole.hide();
   });
+  closeButton.show();
 
   //get the level of amplitude of the mic
   let level = amp.getLevel();
@@ -209,13 +339,13 @@ function screen2() {
     noStroke();
     fill(255,255,255);
     textStyle(BOLD);
-    textSize(50);
+    textSize(70);
     textAlign(CENTER, CENTER);
-    textFont('Verdana');
+    textFont('VT323');
 
-    text("Let your voice \n be seen!", width/2, height/4);
+    text("Let your voice\nbe seen!", width/2, height/4);
 
-    textSize(30);
+    textSize(50);
     text("Maintain your volume above the line\nto see your color expand", width/2, 100 + 3*height/4);
     
   pop();
